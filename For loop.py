654bed11c7f3ab0a2b3a281df6e2ae43ddfaf237
Syntax Error <157401490+Samir3vil@users.noreply.github.com>
2024-11-03 14:00:38 +0530

    .

---
+++ For loop.py
@@ -0,0 +1,7 @@
+# fruits = "Syntax Error"
+
+# for i in fruits:
+#     print(i)
+# range (start,stop,step)
+for i in range(0,11,3):
+    print(i)
---
+++ While Loop/While loop.py
@@ -0,0 +1,10 @@
+
+count=11
+
+while count>=10:
+    count= count + 1
+    if count==5:
+        break
+    print(count)
+    
+   
