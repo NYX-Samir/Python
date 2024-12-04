319a1ecea079045c4a9e95f24ecf7402d20fc483
Syntax Error <157401490+Samir3vil@users.noreply.github.com>
2024-12-05 00:18:56 +0530

    tuple

---
+++ Dictionary.py
@@ -0,0 +1,17 @@
+dict ={
+    "name":"Syntax Error",
+    "Age":22,
+    "Topic":"Dictonary"
+}
+
+# print(dict.keys())
+# print(dict.values())
+# print(dict.update({"views":100000}))
+# print(dict)
+# dict.pop("Topic")
+# print(dict)
+# del dict
+# print(dict)
+
+dict["Age"]=30
+print(dict)
---
+++ Tuples.py
@@ -0,0 +1,8 @@
+'''numbers = (1,2,3)
+print(numbers[1:3])
+print(numbers)
+for items in numbers:
+    print(items)'''
+numbers = (1,2,3)  
+first ,second ,third = numbers
+print(third)
